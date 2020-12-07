118eb1a5308c6ceec7af6bbea85a08ee768ece1f
Timor Raiman <timor.raiman@intel.com>
2020-12-07 18:45:58 +0200

    Temporary poc - use UDP instead of TCP

--- src/main/java/jenkins/plugins/logstash/persistence/LogstashDao.java
+++ src/main/java/jenkins/plugins/logstash/persistence/LogstashDao.java
@@ -1,9 +1,10 @@
 package jenkins.plugins.logstash.persistence;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class LogstashDao extends HostBasedLogstashIndexerDao {
 
@@ -13,14 +14,14 @@ public class LogstashDao extends HostBasedLogstashIndexerDao {
 
   @Override
   public void push(String data) throws IOException {
+    byte[] buf = (data+"\n").getBytes(StandardCharsets.UTF_8);
 
-    try (Socket logstashClientSocket = new Socket(getHost(), getPort()))
+    try(DatagramSocket socket = new DatagramSocket())
     {
-      OutputStream out = logstashClientSocket.getOutputStream();
-      out.write(data.getBytes(StandardCharsets.UTF_8));
-      out.write(10);
-      out.flush();
-      out.close();
+      DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(getHost()), getPort());
+      socket.send(packet);
+      socket.close();
     }
   }
 }
+
